2bb8ce18ca2ad0a1691a65cf1c8476825edd55ba
fidelisluc4sm <luckas_jf@hotmail.com>
2024-12-28 01:12:34 -0300

    Revert "1.1.0 Contruindo o Cliente para solicitações HttpClient"
    This reverts commit f4993bf150b337a5e960b076aa718c3d879de9a7.

--- Converso-de-Moeda/src/Main.java
+++ Converso-de-Moeda/src/Main.java
@@ -1,26 +1,5 @@
-import br.com.alura.conversor.model.ConsultaMoeda;
-import br.com.alura.conversor.model.Moeda;
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-
-        ConsultaMoeda m = new ConsultaMoeda();
-
-        //String jsonResposta = m.buscaMoeda("BRL");
-
-        //System.out.println(jsonResposta);
-
-        try {
-            Moeda moe = m.buscaMoeda("BRL");
-            //System.out.println(moe);
-            System.out.println("base do codigo: "+moe.base_code());
-            System.out.println("taxa de conversao para eur: "+moe.conversion_rates().EUR());
-            System.out.println("ultima atualização: "+moe.time_last_update_utc());
-        } catch (Exception e) {
-            throw new RuntimeException(e.getMessage());
-        }
 
     }
 }
